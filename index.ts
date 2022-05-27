c1640fe61f58bc6c1d744188456dd2a5962461ca
jose.loureiro <jose.loureiro@olx.com>
2022-05-27 11:00:21 +0100

    Remove all FE Remove all FE from template of heroku. Keep just a hello world on /

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import express from "express";
 
 const app = express();
-const port = 5000; // default port to listen
+const PORT = process.env.PORT || 5000; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
